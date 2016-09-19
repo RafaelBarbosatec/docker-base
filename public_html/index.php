9a850310ce8a9453baa47ddc854d1ce253cec1ac
Rafael <rafaelbarbosatec@gmail.com>
2016-09-19 09:17:53 -0300

    removida alguns itens e add assinatura ssl

--- public_html/index.php
+++ public_html/index.php
@@ -7,22 +7,7 @@
     <body>
         <img src="http://blog.osteel.me/images/2015/12/18/docker-tutorial2.gif" alt="Hello World!" />
         <?php
-        $database   = $user = $password = "project";
-        $host       = "mysql";
-        $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
-        $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
-        $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
-
-        if (empty($tables)) {
-            echo "<p>There are no tables in database \"{$database}\".</p>";
-        } else {
-            echo "<p>Database \"{$database}\" has the following tables:</p>";
-            echo "<ul>";
-            foreach ($tables as $table) {
-                echo "<li>{$table}</li>";
-            }
-            echo "</ul>";
-        }
+       phpinfo();
         ?>
     </body>
 </html>
