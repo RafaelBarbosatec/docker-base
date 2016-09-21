8706844b1897870b43f1fd8c3ceff5d1b7a0d3b3
root <rafaelbarbosatec@gmail.com>
2016-09-21 12:29:07 -0300

    ajustes

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,8 +7,16 @@
     </head>
     <body>
         <img src="http://blog.osteel.me/images/2015/12/18/docker-tutorial2.gif" alt="Hello World!" />
+<br/>
         <?php
-       phpinfo();
+       $conn = mysqli_connect("mysql", "root", "root") or die(mysqli_error());
+		echo "Conectado na base<br />";
+		$res = mysqli_query($conn, "SHOW DATABASES");
+		echo "<pre>";
+		while($row = mysqli_fetch_assoc($res)){
+			print_r($row);
+		}
+		echo "</pre>";
         ?>
     </body>
 </html>
